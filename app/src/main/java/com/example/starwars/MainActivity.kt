b853557ec6254f344c27b387ab2ec5e20246dd9e
tejaskhartude <tejaskhartude@gmail.com>
2022-05-31 12:47:45 +0530

    updated

--- app/src/main/java/com/example/starwars/MainActivity.kt
+++ app/src/main/java/com/example/starwars/MainActivity.kt
@@ -2,28 +2,42 @@ package com.example.starwars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.starwars.Viewmodel.StarViewmodel
+import com.example.starwars.api.RetrofitInstance
+import com.example.starwars.api.StarwarsAdapter
 import com.example.starwars.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var viewModel:StarViewmodel()
+    lateinit var viewModel:StarViewmodel
     private lateinit var binding: ActivityMainBinding
+    lateinit var starwarsAdapter: StarwarsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-         setUpRecyclerView()
         viewModel = ViewModelProvider(this).get(StarViewmodel::class.java)
+         setUpRecyclerView()
+
+        binding.progressBar.isVisible = true
+        
 
     }
 
-    private fun setUpRecyclerView() = binding.recyclerCharacters.apply{
+    private  fun setUpRecyclerView() = binding.recyclerCharacters.apply{
+                   starwarsAdapter = StarwarsAdapter()
+                    adapter = starwarsAdapter
+                    layoutManager = LinearLayoutManager(this@MainActivity)
+
 
     }
 }
--- app/src/main/java/com/example/starwars/Viewmodel/StarViewmodel.kt
+++ app/src/main/java/com/example/starwars/Viewmodel/StarViewmodel.kt
@@ -1,15 +1,27 @@
 package com.example.starwars.Viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.starwars.StarRepository.StarwarsRepository
+import com.example.starwars.model.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import okhttp3.Response
 
 class StarViewmodel(private val starwarsRepository: StarwarsRepository):ViewModel() {
 
+       val characters:LiveData<Result> = starwarsRepository.getCharacters()
+    /*
     suspend fun getCharacters() = viewModelScope.launch {
-        val character_names = starwarsRepository.getCharacters()
-    }
+         starwarsRepository.getCharacters()
+    }*/
+
+        fun getCharacters() {
+           val characters:Flow<List<Result>> = starwarsRepository.getCharacters()
+       }
 
 
 
