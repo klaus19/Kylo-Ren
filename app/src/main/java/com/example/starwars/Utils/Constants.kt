2a0ee173ab1cf9e35ea49db757b0b91441b46cd0
tejaskhartude <tejaskhartude@gmail.com>
2022-06-02 14:21:53 +0530

    Updated

--- app/src/main/java/com/example/starwars/Utils/Constants.kt
+++ app/src/main/java/com/example/starwars/Utils/Constants.kt
@@ -3,6 +3,6 @@ package com.example.starwars.Utils
 object Constants {
 
     const val BASE_URL = "https://swapi.dev/api/"
-    const val FIRST_PAGE_INDEX = 1
-    const val NETWORK_PAGE_SIZE = 30
+  //  const val FIRST_PAGE_INDEX = 1
+  //  const val NETWORK_PAGE_SIZE = 30
 }
